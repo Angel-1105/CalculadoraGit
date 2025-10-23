5e0c50d6f9e6cb9078d4bc7f35477679158ecd45
Ángel Molina <amolred1105@g.educaand.es>
2025-10-23 12:15:10 +0200

    Añadida la división con control de error

--- src/com/ejemplo/calculadora/Calculadora.java
+++ src/com/ejemplo/calculadora/Calculadora.java
@@ -9,5 +9,11 @@ public class Calculadora {
 	}
 	public double multiplicar (double a, double b) {
 	    return a * b;
-}
+	}
+	public double dividir(double a, double b) {
+		if (b == 0) {
+		throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
+		return a / b;
+	}
 }
